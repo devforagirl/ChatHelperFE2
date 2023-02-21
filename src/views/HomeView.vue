05cf0cfb27cdd37cda5a3e11d8b3c7f77cd310d5
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-21 08:38:12 +0000

    update e

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Chat Helper d</h1>
+    <h1>Chat Helper e</h1>
     <div v-if="!socketConnected">
       <button @click="connectSocket">Connect</button>
     </div>
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-import io from 'socket.io-client';
+import io from 'socket.io-client'
 
 export default {
   name: 'HomeView',
@@ -21,22 +21,22 @@ export default {
   data() {
     return {
       socketConnected: false,
-    };
+    }
   },
   methods: {
     connectSocket() {
-      console.log('process.env.VUE_APP_BACKEND_URL d->', process.env.VUE_APP_BACKEND_URL);
-      this.socket = io(process.env.VUE_APP_BACKEND_URL);
+      console.log('process.env.VUE_APP_BACKEND_URL e->', process.env.VUE_APP_BACKEND_URL)
+      this.socket = io(process.env.VUE_APP_BACKEND_URL)
       this.socket.on('connect', () => {
-        console.log('Connected to server');
-        this.socketConnected = true;
-      });
+        console.log('Connected to server')
+        this.socketConnected = true
+      })
     },
     disconnectSocket() {
-      this.socket.disconnect();
-      this.socketConnected = false;
-      console.log('Disconnected from server');
+      this.socket.disconnect()
+      this.socketConnected = false
+      console.log('Disconnected from server')
     },
   },
-};
+}
 </script>
