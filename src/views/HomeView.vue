0da9407e07614670c858a4d324c22d3b199b8749
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-03-12 13:44:57 +0800

    用FE了
    用FE了

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,9 +8,19 @@
       <button @click="disconnectSocket">Disconnect</button>
     </div>
 
+    <br />
     <div>
-      <button @click="btnClick1">btnClick1</button>
+      <button @click="btnClick1">message</button>
     </div>
+    <br />
+    <div>
+      <button @click="btnClick2">startProcess</button>
+    </div>
+    <br />
+    <div>
+      <button @click="btnClick2">createChatObject</button>
+    </div>
+    <br />
   </div>
 </template>
 
@@ -38,17 +48,34 @@ export default {
       this.socket.on('response', () => {
         console.log('response +1')
       })
+
+      this.socket.on('chatsData', (r) => {
+        console.log('chatsData +1', r)
+      })
+
+      this.socket.on('exceptionInfo', (e) => {
+        console.log('exceptionInfo +1', e)
+      })
     },
     disconnectSocket() {
       this.socket.disconnect()
       this.socketConnected = false
       console.log('Disconnected from server')
     },
-
     btnClick1(){
       console.log('btnClick1')
       this.socket.emit('message','btnClick1')
+    },
+    btnClick2(){
+      console.log('btnClick2')
+      this.socket.emit('startProcess')
+    },
+    btnClick3(){
+       console.log('btnClick3')
+  this.socket.emit('createChatObject', (fb) => {
+    console.log('fb:', fb)
+  })
     }
-  },
+  }
 }
 </script>
